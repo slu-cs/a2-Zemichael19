c8208de2fa97611b7e2acb3f4bae9298794116a7
Zemichael Habtegiorgis <zemichaelhabtegiorgis@Zemichaels-MacBook-Pro.local>
2020-02-22 18:40:58 -0500

    changes

--- Votingmongo/query.js
+++ Votingmongo/query.js
@@ -8,26 +8,15 @@ connect(); // To the database
 
 const queries = [
   // What are names in alphabetical order?
-  Voters.find().where('zipcode').equals('13617').count()
+  Voters.find().where('zipcode').equals('13617').countDocuments(),
+  Voters.find().where('first').equals('STARR')
 
-  /* Who started most recently?
-  Professor.find().sort('-started').limit(1),
 
-  // Who started in 2003?
-  Professor.find().where('started').equals(2003),
-
-  // Who teaches 362?
-  Professor.find().where('courses').in(362),
-
-  // What are all the ranks?
-  Professor.distinct('rank')
-
-  */
 ];
-
 
 // Run the queries in parallel
 Promise.all(queries)
   .then(function(result) {
     console.log('How many registered voters live in the Canton zipcode? ', result[0]);
+    console.log('What are the full names of all registered voters whose full name is STARR?', result[1].map(x => x.first + x.last));
 }).catch(error => console.error(error.stack));
