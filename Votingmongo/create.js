2bfe76305f990c4155af7f06731f50233cc0cd63
Zemichael Habtegiorgis <zemichaelhabtegiorgis@Zemichaels-MacBook-Pro.local>
2020-02-22 20:17:13 -0500

    changes

--- Votingmongo/create.js
+++ Votingmongo/create.js
@@ -27,10 +27,10 @@ file.on('line', function(line) {
   )
 });
 
-file.on('line', function(line)) {
+file.on('close', function(line) {
   mongoose.connection.dropDatabase()
     .then(() => Promise.all(result.map(x => x.save())))
     .then(() => mongoose.connection.close())
     .then(()=> console.log("Ready"))
     .catch(error => console.error(error.stack));
-}
+})
