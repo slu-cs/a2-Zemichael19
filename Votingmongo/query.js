adccfd74634ddc8e21553310159e578aca9762b8
Zemichael Habtegiorgis <zemichaelhabtegiorgis@Zemichaels-MacBook-Pro.local>
2020-02-23 15:33:48 -0500

    changes

--- Votingmongo/query.js
+++ Votingmongo/query.js
@@ -23,5 +23,5 @@ Promise.all(queries)
     console.log('What are the full names of all registered voters whose first name is STARR?', result[1].map(d => d.first +"-" +d.last));
     console.log('How many people voted in the 2016 general election (GE16)?', result[2])
     console.log('What is the last-name that comes last in the county in alphabetical order?', result[3].map(d => d.last))
-    console.log('How many zip codes does the county contain?', result[4].length)
+    console.log('How many zip codes does the county contain?', result[4])
 }).catch(error => console.error(error.stack));
