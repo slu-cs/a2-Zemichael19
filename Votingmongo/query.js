c43dac15384f00667b4a562c5d85bdb64fff56b1
Zemichael Habtegiorgis <zemichaelhabtegiorgis@Zemichaels-MacBook-Pro.local>
2020-02-22 19:14:33 -0500

    changes

--- Votingmongo/query.js
+++ Votingmongo/query.js
@@ -8,7 +8,7 @@ connect(); // To the database
 
 const queries = [
 
-  Voters.find().where('zipcode').count(),
+  Voters.find().where('zipcode').equals(13617).count(),
   Voters.find().where('first').equals('STARR'),
 
 
@@ -18,5 +18,5 @@ const queries = [
 Promise.all(queries)
   .then(function(result) {
     console.log('How many registered voters live in the Canton zipcode? ', result[0]);
-    console.log('What are the full names of all registered voters whose full name is STARR?', result[1].map(x => x.first + x.last));
+    console.log('What are the full names of all registered voters whose full name is STARR?', result[1].map(x => x.first +"-" x.last));
 }).catch(error => console.error(error.stack));
